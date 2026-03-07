f54563620ae1e944eead2fb7763f89979607f0fa
Giri6126 <girigiri6126@gmail.com>
2026-03-07 18:09:21 +0530

    Add files via upload

--- RIT Live Location/config.py
+++ RIT Live Location/config.py
@@ -5,13 +5,12 @@ from mysql.connector import Error
 def get_db_connection():
     """
     Returns a MySQL database connection.
-    Raises an exception if connection fails.
     """
     try:
         conn = mysql.connector.connect(
             host="localhost",
-            user="root",         # replace with your MySQL username
-            password="Giri2006@",         # replace with your MySQL password
+            user="root",
+            password="Giri2006@",
             database="rit_transport"
         )
         return conn
